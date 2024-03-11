9ebc973a53884732ea498122ec5fe7827f2d46bb
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-03-11 08:28:10 +0200

    I finished

--- index.js
+++ index.js
@@ -1,68 +1,66 @@
 function createNode(value) {
-    return {
-        value,
-        next: null,
-    }
+  return {
+    value,
+    next: null,
+  };
 }
 
 function linkedList() {
-    return {
-        head: null,
+  return {
+    head: null,
 
-        push(value) {
-            const newNode = createNode(value);
-            if (this.head === null) {
-                this.head = newNode;
-            } else {
-                let lastNode = this.head;
-                while (lastNode.next !== null) {
-                    lastNode = lastNode.next;
-                }
-                    lastNode.next = newNode
-                }
-            },
-        
-        
-        pop(value) {
-        if (this.head === null) {
-            return;
-            }
-
-            if (this.head.value === value) {
-                this.head = this.head.next;
-                return;
-            }
-
-            let currentNode = this.head;
-            if (currentNode !== null && currentNode.next.value !== value) {
-                currentNode = currentNode.next
-            }
-
-            if (currentNode.next !== null) {
-               
-                currentNode.next = currentNode.next.next;
-            }
+    push(value) {
+      const newNode = createNode(value);
+      if (this.head === null) {
+        this.head = newNode;
+      } else {
+        let lastNode = this.head;
+        while (lastNode.next !== null) {
+          lastNode = lastNode.next;
+        }
+        lastNode.next = newNode;
+      }
     },
 
-        display() {
-            let currentNode = this.head;
-            while (currentNode.next !== null) {
-                console.log(currentNode.value)
-                currentNode = currentNode.next
-            }
-        }
-    }   
+    pop(value) {
+      if (this.head === null) {
+        return;
+      }
+
+      if (this.head.value === value) {
+        this.head = this.head.next;
+        return;
+      }
+
+      let currentNode = this.head;
+      if (currentNode !== null && currentNode.next.value !== value) {
+        currentNode = currentNode.next;
+      }
+
+      if (currentNode.next !== null) {
+        currentNode.next = currentNode.next.next;
+      }
+    },
+
+    display() {
+      let currentNode = this.head;
+      while (currentNode.next !== null) {
+        console.log(currentNode.value);
+        currentNode = currentNode.next;
+      }
+    },
+  };
 }
 
 const myList = linkedList();
-myList.push(1);
-myList.push(2);
-myList.push(3);
-myList.push(4);
-myList.push(5);
-myList.push(6);
-console.log('Lista Original: ');
-myList.display()
+myList.push('Maria');
+myList.push('Joana');
+myList.push('Bernardino');
+myList.push('Mariazinha');
+myList.push('Rafael');
+myList.push('Albertino');
+console.log('The original list: ');
+myList.display();
 myList.pop(1);
-console.log('Nova lista ficou assim: ')
+console.log('New list is this: ');
 myList.display();
